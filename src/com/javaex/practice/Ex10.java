a4cd5849d9ada2e6e137e02da8628eb2968e5776
djfksdl <djfksdl@gmail.com>
2023-12-29 17:26:12 +0900

    23-12-29

--- src/com/javaex/practice/Ex10.java
+++ src/com/javaex/practice/Ex10.java
@@ -7,28 +7,28 @@ public class Ex10 {
 	public static void main(String[] args) {
 		// 정수 다섯개를 입력받아 가장 큰 수를 출력하세요.
 		
-		Scanner sc = new Scanner(System.in);
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("숫자를 입력하세요");
+//		
+//		System.out.println("숫자:");
+//		int num1 = sc.nextInt();
+//		
+//		System.out.println("숫자:");
+//		int num2 = sc.nextInt();
+//		
+//		System.out.println("숫자:");
+//		int num3 = sc.nextInt();
+//		
+//		System.out.println("숫자:");
+//		int num4 = sc.nextInt();
+//		
+//		System.out.println("숫자:");
+//		int num5 = sc.nextInt();
+//		
+//		while(num)
 		
-		System.out.println("숫자를 입력하세요");
-		
-		System.out.println("숫자:");
-		int num1 = sc.nextInt();
-		
-		System.out.println("숫자:");
-		int num2 = sc.nextInt();
-		
-		System.out.println("숫자:");
-		int num3 = sc.nextInt();
-		
-		System.out.println("숫자:");
-		int num4 = sc.nextInt();
-		
-		System.out.println("숫자:");
-		int num5 = sc.nextInt();
-		
-		
-		
-		sc.close();
+//		sc.close();
 	}
 
 }
