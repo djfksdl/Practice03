d27ea2bb1f3bfe021e9681f154d28b4825706f6d
djfksdl <djfksdl@gmail.com>
2023-12-29 18:13:20 +0900

    23-12-29

---
+++ src/com/javaex/practice/Ex11.java
@@ -0,0 +1,24 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요");
+		int num = sc.nextInt();
+		int sum =0;
+		
+		if(num%2 == 0) {
+			for(int i = num; i>=0; i--) {
+				sum = sum + i;
+				System.out.println(sum);
+			}
+		}
+		
+		sc.close();
+	}
+
+}
